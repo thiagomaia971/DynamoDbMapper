6448177f4523b0f3743404b9a7bb143339259369
Thiago Maia <thiago.maia971@gmail.com>
2023-11-23 15:55:21 -0300

    Feature: select-project and Fix: Select with Nested List

--- src/DynamoDbMapper.Sdk/Repositories/Repository.cs
+++ src/DynamoDbMapper.Sdk/Repositories/Repository.cs
@@ -1,6 +1,8 @@
 ﻿using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.Model;
 using DynamoDbMapper.Sdk.Entities;
+using DynamoDbMapper.Sdk.Extensions;
 using DynamoDbMapper.Sdk.Interfaces;
 
 namespace DynamoDbMapper.Sdk.Repositories;
@@ -10,20 +12,42 @@ public class Repository<T> : IRepository<T>
 {
     protected readonly IDynamoDBContext _dynamoDbContext;
     private readonly MultiTenantScoped _multiTenant;
+    private readonly IAmazonDynamoDB _amazonDynamoDb;
     protected string _entityType;
 
-    public Repository(IDynamoDBContext dynamoDbContext, MultiTenantScoped multiTenant)
+    public Repository(IDynamoDBContext dynamoDbContext, IAmazonDynamoDB amazonDynamoDb, MultiTenantScoped multiTenant)
     {
         _dynamoDbContext = dynamoDbContext;
+        _amazonDynamoDb = amazonDynamoDb;
         _multiTenant = multiTenant;
         _entityType = Activator.CreateInstance<T>().EntityType;
     }
 
     public virtual async Task<T> Save(T entity)
     {
-        if (entity is TenantEntity m)
-            m.UserId = _multiTenant.UserId;
-        await _dynamoDbContext.SaveAsync(entity);
+        var entities = entity.SegregateEntities();
+        
+        foreach (var entityInner in entities)
+        {
+            if (entityInner is TenantEntity m)
+                m.UserId = _multiTenant.UserId;
+
+            var dic = new Dictionary<string, AttributeValue>();
+            var properties = entityInner.GetPropertiesWithoutAttribute<DynamoDBIgnoreAttribute>();
+
+            foreach (var property in properties)
+            {
+                var value = property.GetValue(entityInner);
+                if (value is null)
+                    continue;
+                
+                dic.Add(property.GetCollumnName(), new AttributeValue(value.ToString()));
+            }
+            
+            var tableName = entityInner.GetPropertyWithAttribute<DynamoDBTableAttribute>().TableName;
+            await _amazonDynamoDb.PutItemAsync(new PutItemRequest(tableName, dic));
+        }
+
         return entity;
     }
 
@@ -47,4 +71,11 @@ public class Repository<T> : IRepository<T>
 
     public virtual async Task Remove(T entity) 
         => await _dynamoDbContext.DeleteAsync(entity);
+}
+
+public class Foo {
+    public void M(int i) { }
+    public void M(string s) { }
+    public void M<T>(T t) { }
+    public void M<T>(T t, string s) { }
 }
